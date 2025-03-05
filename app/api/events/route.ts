65f3978f80fee465213d27dfe8e9ed51f76a21a4
Giovanni Maya <giovannimhern@uchicago.edu>
2025-03-05 10:54:10 -0600

    events post method

---
+++ app/api/events/route.ts
@@ -0,0 +1,36 @@
+import { apiPost } from "../database";
+
+/*
+--- Database schema for HTTP requests ---
+id SERIAL PRIMARY KEY,
+title VARCHAR(255) NOT NULL,
+description TEXT,
+location VARCHAR(255),
+event_date TIMESTAMP NOT NULL,
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+slug TEXT UNIQUE NOT NULL
+*/
+
+export async function POST(req: Request, res: Response) {
+  const body = await req.json();
+  const { title, description, location, event_date, slug } = body;
+
+  const query = `INSERT INTO events(title, description, location, event_date, slug)`;
+  const values = [title, description, location, event_date, slug];
+
+  let status, respBody;
+
+  await apiPost(query, values)
+    .then(() => {
+      status = 200;
+      respBody = { message: "Successfully created event" };
+    })
+    .catch((err) => {
+      status = 400;
+      respBody = err;
+    });
+
+    return Response.json(respBody, {
+        status
+    })
+}
