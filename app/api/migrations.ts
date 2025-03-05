b33730d315eab8d064b4b11188f741e47a485f6b
Giovanni Maya <giovannimhern@uchicago.edu>
2025-03-05 00:25:05 -0600

    db init

---
+++ app/api/migrations.ts
@@ -0,0 +1,24 @@
+import { db } from "./database";
+
+export const migrate = () => {
+    db.serialize(() => {
+        db.run(
+            `
+            CREATE TABLE events (
+                id SERIAL PRIMARY KEY,
+                title VARCHAR(255) NOT NULL,
+                description TEXT,
+                location VARCHAR(255),
+                event_date TIMESTAMP NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            );
+            `
+        ), 
+        (err: Error) => {
+            if (err){
+                console.error(err.message)
+            }
+            console.log("Events table created successfully!")
+        }
+    })
+}
