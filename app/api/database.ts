ab47ac97b98a12db8b4b5ed5b19b2a58a95e5813
Giovanni Maya <giovannimhern@uchicago.edu>
2025-03-05 00:24:43 -0600

    get and post api req with db

--- app/api/database.ts
+++ app/api/database.ts
@@ -1,10 +1,40 @@
+import { rejects } from "assert";
 import path from "path";
 import sqlite3 from "sqlite3";
 
-const dbPath = path.join(process.cwd(), "events.db")
-export const db = new sqlite3.Database(dbPath, sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
-    if(err) {
-        console.error(err.message)
-    } 
-    console.log("Connected successfully to the database!")
-})
+const dbPath = path.join(process.cwd(), "events.db");
+export const db = new sqlite3.Database(
+  dbPath,
+  sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE,
+  (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log("Connected successfully to the database!");
+  }
+);
+
+export const apiGet = async (query: string) => {
+  return await new Promise((resolve, reject) => {
+    db.all(query, (err: Error, row) => {
+      if (err) {
+        console.log(err);
+        return reject(err);
+      }
+      return resolve(row);
+    });
+  });
+};
+
+
+export const apiPost = async (query: string, values: string[]) => {
+    return await new Promise((resolve, reject) => {
+        db.run(query, values, function(err) {
+            if(err){
+                console.log(err)
+                reject(err)
+            }
+            resolve(null)
+        })
+    })
+}
