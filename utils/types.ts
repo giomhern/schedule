47bfe334f1ceae62a58171405e68962b482c592d
Giovanni Maya <giovannimhern@uchicago.edu>
2025-03-05 12:17:33 -0600

    zod event form field types

---
+++ utils/types.ts
@@ -0,0 +1,20 @@
+import { FieldError, UseFormRegister } from "react-hook-form";
+
+export type EventFormData = {
+  title: string;
+  description: string;
+  location: string;
+  event_date: string;
+  slug?: string;
+};
+
+export type FormFieldProps = {
+  type: string;
+  placeholder: string;
+  name: ValidFieldNames;
+  register: UseFormRegister<EventFormData>;
+  error: FieldError | undefined;
+  valueAsNumber?: boolean;
+};
+
+export type ValidFieldNames = "title" | "description" | "location" | "event_date" | "slug";
