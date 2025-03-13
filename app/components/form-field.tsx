5d009bdcdd1d65f5adb60020457c76eea0c15371
Giovanni Maya <giovannimhern@uchicago.edu>
2025-03-12 23:15:05 -0500

    add textarea for long input - spec for desc

--- app/components/form-field.tsx
+++ app/components/form-field.tsx
@@ -1,5 +1,6 @@
 import { FormFieldProps } from "@/utils/types";
 import { motion } from "motion/react";
+
 const FormField: React.FC<FormFieldProps> = ({
   type,
   placeholder,
@@ -9,14 +10,24 @@ const FormField: React.FC<FormFieldProps> = ({
   valueAsNumber,
 }) => {
   return (
-    <>
+    <div className="flex flex-col">
+      <h3 className="mb-2 capitalize font-medium text-sm">{name}</h3>
+      {type === "textbox" ? (
+        <textarea
+          className="py-3 px-3 shadow-xs border border-slate-200 rounded-md text-sm h-32 placeholder:text-sm focus:border-0 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-white"
+          placeholder={placeholder}
+          {...register(name, { valueAsNumber: valueAsNumber })}
+        ></textarea>
+      ) : (
+        <input
+          type={type}
+          placeholder={placeholder}
+          className="py-3 px-3 shadow-xs border border-slate-200 rounded-md text-sm placeholder:text-sm focus:border-0 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-white"
+          {...register(name, { valueAsNumber })}
+        />
+      )}
       {error && <motion.span>{error.message}</motion.span>}
-      <input
-        type={type}
-        placeholder={placeholder}
-        {...register(name, { valueAsNumber })}
-      ></input>
-    </>
+    </div>
   );
 };
 
