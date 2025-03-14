e3e3f89d2a545098507332134fe9b5d56e281a95
Giovanni Maya <giovannimhern@uchicago.edu>
2025-03-14 18:17:04 -0500

    create ZodType object

--- utils/types.ts
+++ utils/types.ts
@@ -1,10 +1,11 @@
 import { FieldError, UseFormRegister } from "react-hook-form";
+import { ZodType, z } from "zod";
 
 export type EventFormData = {
   title: string;
   description: string;
   location: string;
-  event_date: string;
+  event_date?: string;
   slug?: string;
 };
 
@@ -23,3 +24,11 @@ export type ValidFieldNames =
   | "location"
   | "event_date"
   | "slug";
+
+export const EventSchema: ZodType<EventFormData> = z.object({
+  title: z.string().min(1, { message: "Title cannot be empty." }),
+  description: z.string().min(1, { message: "Description cannot be empty." }),
+  location: z.string().min(1, { message: "Location cannot be empty." }),
+  event_date: z.string().min(1, { message: "Event date cannot be empty." }),
+  slug: z.string().min(1, { message: "Slug cannot be empty." }),
+});
