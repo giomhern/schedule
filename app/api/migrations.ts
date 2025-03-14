f327ddfce982d265dd867f76485bccc354085f0d
Giovanni Maya <giovannimhern@uchicago.edu>
2025-03-14 18:17:29 -0500

    migrate users database

--- app/api/migrations.ts
+++ app/api/migrations.ts
@@ -4,16 +4,16 @@ export const migrate = () => {
   db.serialize(() => {
     db.run(
       `
-            CREATE TABLE events (
-                id SERIAL PRIMARY KEY,
-                title VARCHAR(255),
-                description TEXT,
-                location VARCHAR(255),
-                event_date TEXT,
-                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
-                slug TEXT
-            );
-            `
+      CREATE TABLE IF NOT EXISTS events (
+          id SERIAL PRIMARY KEY,
+          title VARCHAR(255),
+          description TEXT,
+          location VARCHAR(255),
+          event_date TEXT,
+          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+          slug TEXT
+      );
+      `
     ),
       (err: Error) => {
         if (err) {
@@ -24,4 +24,26 @@ export const migrate = () => {
   });
 };
 
-migrate()
+export const runMigrations = () => {
+  db.serialize(() => {
+    db.run(
+      `
+      CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        emailId TEXT UNIQUE NOT NULL,
+        password TEXT NOT NULL
+      );
+      `,
+      (err: Error) => {
+        if (err) {
+          console.error(err.message);
+        }
+        console.log("Users table created successfully.");
+      }
+    );
+  });
+};
+
+
+runMigrations()
